3b6ebecb202ea640cbf8fda6abbc8a2404158490
abhishekdegaonkar <abhishekdegaonkar@gmail.com>
2026-08-04 14:52:43 +0530

    Create gift.js

---
+++ gift.js
@@ -0,0 +1,13 @@
+const gift=document.getElementById("giftBox");
+
+gift.addEventListener("click",()=>{
+
+gift.classList.add("open");
+
+setTimeout(()=>{
+
+window.location.href="celebration.html";
+
+},1200);
+
+});
